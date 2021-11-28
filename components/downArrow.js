e97558bcce47feb76bd7cb729ba3f9e7bb817773
Somnath Prasad <somnathprasad559@gmail.com>
2021-11-29 00:02:38 +0530

    initial commit

---
+++ components/downArrow.js
@@ -0,0 +1,15 @@
+export default function DownArrow() {
+  return (
+    <div className="absolute bottom-10 animate-bounce">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="white"
+      >
+        <path d="M16 17l-4 4m0 0l-4-4m4 4V3" />
+      </svg>
+    </div>
+  );
+}
